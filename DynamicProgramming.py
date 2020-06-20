ef3458aae7deae59e06b008e4d2bc2b8c02f2131
limingzhou23 <850073458@qq.com>
2020-06-20 10:12:42 +0800

    动态规划

---
+++ DynamicProgramming.py
@@ -0,0 +1,35 @@
+#10. 正则表达式匹配
+# 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+#
+# '.' 匹配任意单个字符
+# '*' 匹配零个或多个前面的那一个元素
+#使用动态规划的方法
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+
+        def matches(i: int, j: int) -> bool:
+            if i == 0:
+                return False
+            if p[j - 1] == '.':
+                return True
+            return s[i - 1] == p[j - 1]
+
+        f = [[False] * (n + 1) for _ in range(m + 1)]
+        f[0][0] = True
+        for i in range(m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    f[i][j] |= f[i][j - 2]
+                    if matches(i, j - 1):
+                        f[i][j] |= f[i - 1][j]
+                else:
+                    if matches(i, j):
+                        f[i][j] |= f[i - 1][j - 1]
+        return f[m][n]
+
+
+solution = Solution()
+s = "mississippi"
+p = "mis*is*p*."
+print(solution.isMatch(s,p))
